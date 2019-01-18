45c7c890613de3bc6eae7ff82cef42a9ecd0a5d3
Harshdeep Gupta <hag@microsoft.com>
2019-01-18 15:12:29 -0800

    fix params

--- src/application-insights/azext_applicationinsights/tests/latest/test_applicationinsights_commands.py
+++ src/application-insights/azext_applicationinsights/tests/latest/test_applicationinsights_commands.py
@@ -11,11 +11,11 @@ class ApplicationInsightsDataClientTests(ScenarioTest):
     """Test class for Application Insights data client."""
     def test_query_execute(self):
         """Tests data plane query capabilities for Application Insights."""
-        self.cmd('az monitor app-insights query --apps 578f0e27-12e9-4631-bc02-50b965da2633 f4963800-c77d-40a3-8b0b-448678904c33 --analytics-query "availabilityResults | distinct name | order by name asc"', checks=[
+        self.cmd('az monitor app-insights query --apps 00000000-12e9-4631-bc02-50b965da2633 f4963800-c77d-40a3-8b0b-448678904c33 --analytics-query "availabilityResults | distinct name | order by name asc"', checks=[
             self.check('tables[0].rows[-1][0]', 'microsoft'),
             self.check('tables[0].rows[-2][0]', 'google')
         ])
-        query_guid = self.cmd('az monitor app-insights query --app 578f0e27-12e9-4631-bc02-50b965da2633 --analytics-query "requests | getschema"').get_output_in_json()
+        query_guid = self.cmd('az monitor app-insights query --app 00000000-12e9-4631-bc02-50b965da2633 --analytics-query "requests | getschema"').get_output_in_json()
         query_name_rg = self.cmd('az monitor app-insights query --apps ace-test -g ace-test --analytics-query "requests | getschema"').get_output_in_json()
         query_azure_id = self.cmd('az monitor app-insights query --analytics-query "requests | getschema" --ids /subscriptions/b98981de-4152-480f-a515-59b099299283/resourceGroups/ace-test/providers/microsoft.insights/components/ace-test').get_output_in_json()
         assert query_guid == query_name_rg
@@ -25,12 +25,12 @@ class ApplicationInsightsDataClientTests(ScenarioTest):
 
     def test_metrics_show(self):
         result = self.cmd('az monitor app-insights metrics show --app /subscriptions/b98981de-4152-480f-a515-59b099299283/resourceGroups/ace-test/providers/microsoft.insights/components/ace-test -m availabilityResults/count').get_output_in_json()
-        azure_result = self.cmd('az monitor app-insights metrics show --app 578f0e27-12e9-4631-bc02-50b965da2633 --metric availabilityResults/count').get_output_in_json()
+        azure_result = self.cmd('az monitor app-insights metrics show --app 00000000-12e9-4631-bc02-50b965da2633 --metric availabilityResults/count').get_output_in_json()
         assert isinstance(result["value"]["availabilityResults/count"]['sum'], (int, float, complex))
         assert result["value"]["availabilityResults/count"]['sum'] == azure_result["value"]["availabilityResults/count"]['sum']
 
     def test_metrics_get_metadata(self):
-        self.cmd('az monitor app-insights metrics get-metadata --app 578f0e27-12e9-4631-bc02-50b965da2633', checks=[
+        self.cmd('az monitor app-insights metrics get-metadata --app 00000000-12e9-4631-bc02-50b965da2633', checks=[
             self.check('dimensions."availabilityResult/location".displayName', 'Run location'),
             self.check('metrics."availabilityResults/availabilityPercentage".displayName', 'Availability'),
             self.check('metrics."users/count".supportedAggregations[0]', 'unique'),
@@ -38,16 +38,16 @@ class ApplicationInsightsDataClientTests(ScenarioTest):
         ])
 
     def test_events_show(self):
-        self.cmd('az monitor app-insights events show --app 578f0e27-12e9-4631-bc02-50b965da2633 --type availabilityResults --event 1f492e8f-de80-11e8-8fec-e18ec74f57af', checks=[
-            self.check('value[0].ai.appId', '578f0e27-12e9-4631-bc02-50b965da2633'),
+        self.cmd('az monitor app-insights events show --app 00000000-12e9-4631-bc02-50b965da2633 --type availabilityResults --event 1f492e8f-de80-11e8-8fec-e18ec74f57af', checks=[
+            self.check('value[0].ai.appId', '00000000-12e9-4631-bc02-50b965da2633'),
             self.check('value[0].availabilityResult.duration', 104),
             self.check('value[0].client.city', 'Boydton')
         ])
 
     def test_events_list(self):
-        self.cmd('az monitor app-insights events list --timespan PT5M --app 578f0e27-12e9-4631-bc02-50b965da2633 --type availabilityResults', checks=[
-            self.check('value[0].ai.appId', '578f0e27-12e9-4631-bc02-50b965da2633'),
+        self.cmd('az monitor app-insights events list --timespan PT5M --app 00000000-12e9-4631-bc02-50b965da2633 --type availabilityResults', checks=[
+            self.check('value[0].ai.appId', '00000000-12e9-4631-bc02-50b965da2633'),
         ])
-        result = self.cmd('az monitor app-insights events list --timespan PT5M --app 578f0e27-12e9-4631-bc02-50b965da2633 --type availabilityResults').get_output_in_json()
+        result = self.cmd('az monitor app-insights events list --timespan PT5M --app 00000000-12e9-4631-bc02-50b965da2633 --type availabilityResults').get_output_in_json()
         assert isinstance(result["value"][0]["client"]["city"], ("".__class__, u"".__class__))
         assert isinstance(result["value"][0]["availabilityResult"]["duration"], (int, float, complex))
